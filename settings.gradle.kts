3beabf765405c54fb72066fe4536512cf58c84f0
MSI\Blues <truongnhlse160191@fpt.edu.vn>
2023-10-26 20:27:33 +0700

    initilize project

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BIRD_FARM_SHOP_ANDROID"
+include(":app")
+ 
