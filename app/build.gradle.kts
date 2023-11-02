7b0c2dcca461475fb60b95aab511714ca1b08e3b
tomlenek <lequangminh2204@gmail.com>
2023-11-02 08:10:14 +0700

    Update layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,17 @@ android {
 
 dependencies {
 
+//    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("com.google.android.material:material:1.10.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+////    implementation(files("C:/Users/Blues/Desktop/PRM_TTTM/jtds-1.3.1.jar"))
+//    implementation(files("E:\\Semester\\Term7\\PRM392\\BIRD_FARM_SHOP_ANDROID\\jtds-1.3.1.jar"))
+//    implementation("com.google.firebase:firebase-auth:22.2.0")
+//    implementation("com.google.firebase:firebase-database:20.3.0")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")'
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -43,5 +54,4 @@ dependencies {
     implementation("com.google.firebase:firebase-auth:22.2.0")
     implementation("com.google.firebase:firebase-database:20.3.0")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-}
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")}
